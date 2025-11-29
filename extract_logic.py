382ed4ad88bb5f1daad2a62e740321bbfe9bc828
AmanpreetSingh0071 <amanpreet.m.ahluwalia@gmail.com>
2025-11-29 11:27:30 +0000

    New Updates for Langchain

--- extract_logic.py
+++ extract_logic.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pdfplumber
 import os
 from groq import Groq
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
